89725d5accb1af66d23f77f6c594cd4027918228
niujie <1033751979@qq.com>
2020-07-02 20:51:33 +0800

    route require

--- Modules/Blog/Routes/Http/route.php
+++ Modules/Blog/Routes/Http/route.php
@@ -1,13 +1,13 @@
 <?php
 $api = app('Dingo\Api\Routing\Router');
 $api->group(['middleware' => 'slidingGrant'], function($api) {
-    foreach(glob(__DIR__.'/NotVerify/*.php') as $item) {
-        require $item;
+    foreach(glob(__DIR__.'/NotVerify/*.php') as $filename) {
+        empty($filename) ? '' : require $filename;
     }
 });
 
 $api->group(['middleware' => 'auth.token.jwt:api'], function($api) {
-    foreach(glob(__DIR__.'/Verify/*.php') as $item) {
-        require $item;
+    foreach(glob(__DIR__.'/Verify/*.php') as $filename) {
+        empty($filename) ? '' : require $filename;
     }
 });
