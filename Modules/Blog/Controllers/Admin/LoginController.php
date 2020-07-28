373cdcd3ae2db6637ec1e46b051ce462678b4e76
niujie <1033751979@qq.com>
2020-07-28 22:58:58 +0800

    job

--- Modules/Blog/Controllers/Admin/LoginController.php
+++ Modules/Blog/Controllers/Admin/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Modules\Blog\Controllers\Admin;
 
+use App\Jobs\Async;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
@@ -21,9 +22,6 @@ class LoginController extends BaseController
      */
     public function login(Request $request)
     {
-        $res = Redis::set('test12', 123);
-        dd($res);
-        exit;
         $credentials = $request->only(['mobile', 'password']);
 
         if(!$token = auth('admin')->attempt($credentials)) {
@@ -31,5 +29,18 @@ class LoginController extends BaseController
         }
         recordLog(print_r($credentials, true), 'logs/login/info.log');
         return $this->respondWithToken($token);
+    }
+
+    /**
+     * Brief:admin job
+     *
+     * @return \Illuminate\Foundation\Bus\PendingDispatch
+     */
+    public function job()
+    {
+        $job = new Async();
+
+        $res = Async::dispatch($job)->onQueue('async');
+        dd($res);
     }
 }
--- Modules/Blog/Routes/Admin/NotVerify/adminUser.php
+++ Modules/Blog/Routes/Admin/NotVerify/adminUser.php
@@ -1,2 +1,3 @@
 <?php
 $apiAdmin->get('/admin/login', 'LoginController@login');
+$apiAdmin->get('/async/job', 'LoginController@job');
