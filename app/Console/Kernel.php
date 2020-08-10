61832013b0ba33b5f214fa1cf1bda6d4132694be
niujie <1033751979@qq.com>
2020-08-10 15:36:45 +0800

    每10分钟执行一次

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('job')->everyMinute();
+         $schedule->command('job')->everyFiveMinutes();
     }
 
     /**
